abf8e16a3b890eaf2e7bd937721e9009ac4082bc
opilane <opilane@HP-CZC0359NND>
2024-05-16 09:14:13 +0300

    Add project files.

---
+++ ifnesle11341/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ifnesle11341
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vajutage 1");
+            int e = Convert.ToInt16(Console.ReadLine());
+            if(e == 1)
+            {
+                Console.WriteLine("Zesty");
+            }
+            else
+            {
+                Console.WriteLine("WOMP WOMP");
+            }
+        }
+    }
+}
